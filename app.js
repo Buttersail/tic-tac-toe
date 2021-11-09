179230f20f424ac82dcf08d9fcbc5f215b5c0c74
Murat Memis <h.m.f.m.92@gmail.com>
2021-11-09 15:08:38 +0100

    refactoring
    refactored the code so I can actually start coding the logic

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-// Place the mark
-// Check for Win
+// Place the mark X
+// Switch Turns with O
+// Check for Win on either X or O
 // Check for Draw
-// Switch Turns
 
-// Ran out of time because I was too slow on copying and making the CSS work... Stupid mistakes.
+let currentPlayer = 'Appelsinvand'
+
+let gameActive = true
+
+let gameState = ['', '', '', '', '', '', '', '', '']
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+  gameState[clickedCellIndex] = currentPlayer
+  clickedCell.innerText = currentPlayer
+}
+function handleCellClick(clickedCellEvent) {
+  const clickedCell = clickedCellEvent.target
+
+  const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'))
+
+  handleCellPlayed(clickedCell, clickedCellIndex)
+}
+
+//Not my own code
+document.querySelectorAll('.cell').forEach((cell) => cell.addEventListener('click', handleCellClick))
