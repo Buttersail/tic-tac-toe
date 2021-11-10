5fefbd4e719576c07d90aeb0cda418f9eb0187b3
Murat Memis <h.m.f.m.92@gmail.com>
2021-11-10 21:58:32 +0100

    scafolding

--- app.js
+++ app.js
@@ -3,23 +3,49 @@
 // Check for Win on either X or O
 // Check for Draw
 
-let currentPlayer = 'Appelsinvand'
+let currentPlayer = 'X'
 
 let gameActive = true
 
-let gameState = ['', '', '', '', '', '', '', '', '']
-
-function handleCellPlayed(clickedCell, clickedCellIndex) {
-  gameState[clickedCellIndex] = currentPlayer
-  clickedCell.innerText = currentPlayer
-}
-function handleCellClick(clickedCellEvent) {
-  const clickedCell = clickedCellEvent.target
-
-  const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'))
-
-  handleCellPlayed(clickedCell, clickedCellIndex)
+document.querySelector('.cell1').onclick = function () {
+  if (gameActive) {
+    if (currentPlayer === 'X') {
+      document.querySelector('.cell1').innerText = currentPlayer
+      currentPlayer = 'O'
+    } else {
+      document.querySelector('.cell1').innerText = currentPlayer
+    }
+  }
 }
 
-//Not my own code
-document.querySelectorAll('.cell').forEach((cell) => cell.addEventListener('click', handleCellClick))
+document.querySelector('.cell2').onclick = function () {
+  document.querySelector('.cell2').innerText = currentPlayer
+}
+
+document.querySelector('.cell3').onclick = function () {
+  document.querySelector('.cell3').innerText = currentPlayer
+}
+
+document.querySelector('.cell4').onclick = function () {
+  document.querySelector('.cell4').innerText = currentPlayer
+}
+
+document.querySelector('.cell5').onclick = function () {
+  document.querySelector('.cell5').innerText = currentPlayer
+}
+
+document.querySelector('.cell6').onclick = function () {
+  document.querySelector('.cell6').innerText = currentPlayer
+}
+
+document.querySelector('.cell7').onclick = function () {
+  document.querySelector('.cell7').innerText = currentPlayer
+}
+
+document.querySelector('.cell8').onclick = function () {
+  document.querySelector('.cell8').innerText = currentPlayer
+}
+
+document.querySelector('.cell9').onclick = function () {
+  document.querySelector('.cell9').innerText = currentPlayer
+}
