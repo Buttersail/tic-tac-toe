a025fbb98a31d9e9a51be4b1a2d0f085107bc389
Murat Memis <h.m.f.m.92@gmail.com>
2021-11-11 14:05:49 +0100

    add functionality to the board

--- app.js
+++ app.js
@@ -7,45 +7,119 @@ let currentPlayer = 'X'
 
 let gameActive = true
 
+function switchPlayers() {
+  if (currentPlayer === 'X') {
+    currentPlayer = 'O'
+  } else {
+    currentPlayer = 'X'
+  }
+}
+
+//Make it so you cant change the input of the cell after you have entered a mark
 document.querySelector('.cell1').onclick = function () {
   if (gameActive) {
     if (currentPlayer === 'X') {
       document.querySelector('.cell1').innerText = currentPlayer
-      currentPlayer = 'O'
+      switchPlayers()
     } else {
       document.querySelector('.cell1').innerText = currentPlayer
+      switchPlayers()
     }
   }
 }
 
 document.querySelector('.cell2').onclick = function () {
-  document.querySelector('.cell2').innerText = currentPlayer
+  if (gameActive) {
+    if (currentPlayer === 'X') {
+      document.querySelector('.cell2').innerText = currentPlayer
+      switchPlayers()
+    } else {
+      document.querySelector('.cell2').innerText = currentPlayer
+      switchPlayers()
+    }
+  }
 }
 
 document.querySelector('.cell3').onclick = function () {
-  document.querySelector('.cell3').innerText = currentPlayer
+  if (gameActive) {
+    if (currentPlayer === 'X') {
+      document.querySelector('.cell3').innerText = currentPlayer
+      switchPlayers()
+    } else {
+      document.querySelector('.cell3').innerText = currentPlayer
+      switchPlayers()
+    }
+  }
 }
 
 document.querySelector('.cell4').onclick = function () {
-  document.querySelector('.cell4').innerText = currentPlayer
+  if (gameActive) {
+    if (currentPlayer === 'X') {
+      document.querySelector('.cell4').innerText = currentPlayer
+      switchPlayers()
+    } else {
+      document.querySelector('.cell4').innerText = currentPlayer
+      switchPlayers()
+    }
+  }
 }
 
 document.querySelector('.cell5').onclick = function () {
-  document.querySelector('.cell5').innerText = currentPlayer
+  if (gameActive) {
+    if (currentPlayer === 'X') {
+      document.querySelector('.cell5').innerText = currentPlayer
+      switchPlayers()
+    } else {
+      document.querySelector('.cell5').innerText = currentPlayer
+      switchPlayers()
+    }
+  }
 }
 
 document.querySelector('.cell6').onclick = function () {
-  document.querySelector('.cell6').innerText = currentPlayer
+  if (gameActive) {
+    if (currentPlayer === 'X') {
+      document.querySelector('.cell6').innerText = currentPlayer
+      switchPlayers()
+    } else {
+      document.querySelector('.cell6').innerText = currentPlayer
+      switchPlayers()
+    }
+  }
 }
 
 document.querySelector('.cell7').onclick = function () {
-  document.querySelector('.cell7').innerText = currentPlayer
+  if (gameActive) {
+    if (currentPlayer === 'X') {
+      document.querySelector('.cell7').innerText = currentPlayer
+      switchPlayers()
+    } else {
+      document.querySelector('.cell7').innerText = currentPlayer
+      switchPlayers()
+    }
+  }
 }
 
 document.querySelector('.cell8').onclick = function () {
-  document.querySelector('.cell8').innerText = currentPlayer
+  if (gameActive) {
+    if (currentPlayer === 'X') {
+      document.querySelector('.cell8').innerText = currentPlayer
+      switchPlayers()
+    } else {
+      document.querySelector('.cell8').innerText = currentPlayer
+      switchPlayers()
+    }
+  }
 }
 
 document.querySelector('.cell9').onclick = function () {
-  document.querySelector('.cell9').innerText = currentPlayer
+  if (gameActive) {
+    if (currentPlayer === 'X') {
+      document.querySelector('.cell9').innerText = currentPlayer
+      switchPlayers()
+    } else {
+      document.querySelector('.cell9').innerText = currentPlayer
+      switchPlayers()
+    }
+  }
 }
