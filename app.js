b5dba6819c034ea0e60f24bd9a6f07b4b1b9e623
Buttersail <h.m.f.m.92@gmail.com>
2021-11-27 15:30:51 +0100

    The brick wall has been destroyed

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-// Place the mark X --- DONE
-// Switch Turns with O --- DONE
-// Check for Win on either X or O --- DONE(?)
-// Check for Draw
-// CLear board
-
+// Game state
 let currentPlayer = 'X'
-
 let gameActive = true
 
 function switchPlayers() {
-  if (currentPlayer === 'X' && gameActive) {
+  // This is ternary, LEARN IT!
+  // currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
+  if (currentPlayer === 'X') {
     currentPlayer = 'O'
-    document.querySelector('.gameStatus').innerText = 'Player Turn: ' + currentPlayer
   } else {
     currentPlayer = 'X'
-    document.querySelector('.gameStatus').innerText = 'Player Turn: ' + currentPlayer
   }
+  document.querySelector('.gameStatus').innerText = 'Player Turn: ' + currentPlayer
 }
 
 function gameStatus() {
@@ -45,157 +40,53 @@ function gameStatus() {
       document.querySelector('.cell3').innerText === document.querySelector('.cell5').innerText &&
       document.querySelector('.cell5').innerText === document.querySelector('.cell7').innerText)
   ) {
-    switchPlayers() // Calling this to print out who won properly for now.
+    switchPlayers() // Calling this to print out who won properly.
     document.querySelector('.gameStatus').innerText = 'WINNER: ' + currentPlayer
+    gameActive = false
+  } else if (
+    document.querySelector('.cell1').innerText !== '' &&
+    document.querySelector('.cell2').innerText !== '' &&
+    document.querySelector('.cell3').innerText !== '' &&
+    document.querySelector('.cell4').innerText !== '' &&
+    document.querySelector('.cell5').innerText !== '' &&
+    document.querySelector('.cell6').innerText !== '' &&
+    document.querySelector('.cell7').innerText !== '' &&
+    document.querySelector('.cell8').innerText !== '' &&
+    document.querySelector('.cell9').innerText !== ''
+  ) {
+    document.querySelector('.gameStatus').innerText = "It's a draw"
     gameActive = false
   }
 }
 
-// Find a way to find another empty cell since it "skips" if the cell is already occupied
-
-// Find en tilfældig celle
-// Tjek om cellen indeholder noget, hvis ikke sæt enten kryds eller bolle
-// Hvis cellen indeholder noget, skal den finde en ny tilfældig celle
-// Dette skal ske med rekursion, indtil der ikke er flere tomme celler, eller nogen vinder
-
 document.querySelector('.ai').onclick = function () {
-  document.querySelector('.cell' + Math.ceil(Math.random() * 9)).click()
+  if (!gameActive) return null
 
-  if (
-    document.querySelector('.cell1').innerText !== '' ||
-    document.querySelector('.cell2').innerText !== '' ||
-    document.querySelector('.cell3').innerText !== '' ||
-    document.querySelector('.cell4').innerText !== '' ||
-    document.querySelector('.cell5').innerText !== '' ||
-    document.querySelector('.cell6').innerText !== '' ||
-    document.querySelector('.cell7').innerText !== '' ||
-    document.querySelector('.cell8').innerText !== '' ||
-    document.querySelector('.cell9').innerText !== ''
-  ) {
-    document.querySelector('.cell' + Math.ceil(Math.random() * 9)).click()
-    console.log('test')
+  const randomCell = document.querySelector('.cell' + Math.ceil(Math.random() * 9))
+
+  if (randomCell.innerText === '') {
+    randomCell.click()
+  } else {
+    document.querySelector('.ai').onclick()
   }
 }
 
 // Bit ugly code, but it does the job!
 document.querySelector('.reset').onclick = function () {
-  document.querySelector('.cell1').innerText = ''
-  document.querySelector('.cell2').innerText = ''
-  document.querySelector('.cell3').innerText = ''
-  document.querySelector('.cell4').innerText = ''
-  document.querySelector('.cell5').innerText = ''
-  document.querySelector('.cell6').innerText = ''
-  document.querySelector('.cell7').innerText = ''
-  document.querySelector('.cell8').innerText = ''
-  document.querySelector('.cell9').innerText = ''
+  for (let i = 1; i <= 9; i++) {
+    document.querySelector('.cell' + i).innerText = ''
+  }
   currentPlayer = 'X'
   document.querySelector('.gameStatus').innerText = 'Restarted Game - Cleared Board!'
   gameActive = true
 }
 
-document.querySelector('.cell1').onclick = function () {
-  if (gameActive && document.querySelector('.cell1').innerText === '') {
-    if (currentPlayer === 'X') {
-      document.querySelector('.cell1').innerText = currentPlayer
-    } else {
-      document.querySelector('.cell1').innerText = currentPlayer
-    }
-    switchPlayers()
-    gameStatus()
-  }
-}
+for (let i = 1; i <= 9; i++) {
+  document.querySelector('.cell' + i).onclick = function () {
+    if (!gameActive || document.querySelector('.cell' + i).innerText !== '') return null
 
-document.querySelector('.cell2').onclick = function () {
-  if (gameActive && document.querySelector('.cell2').innerText === '') {
-    if (currentPlayer === 'X') {
-      document.querySelector('.cell2').innerText = currentPlayer
-    } else {
-      document.querySelector('.cell2').innerText = currentPlayer
-    }
-    switchPlayers()
-    gameStatus()
-  }
-}
+    document.querySelector('.cell' + i).innerText = currentPlayer
 
-document.querySelector('.cell3').onclick = function () {
-  if (gameActive && document.querySelector('.cell3').innerText === '') {
-    if (currentPlayer === 'X') {
-      document.querySelector('.cell3').innerText = currentPlayer
-    } else {
-      document.querySelector('.cell3').innerText = currentPlayer
-    }
-    switchPlayers()
-    gameStatus()
-  }
-}
-
-document.querySelector('.cell4').onclick = function () {
-  if (gameActive && document.querySelector('.cell4').innerText === '') {
-    if (currentPlayer === 'X') {
-      document.querySelector('.cell4').innerText = currentPlayer
-    } else {
-      document.querySelector('.cell4').innerText = currentPlayer
-    }
-    switchPlayers()
-    gameStatus()
-  }
-}
-
-document.querySelector('.cell5').onclick = function () {
-  if (gameActive && document.querySelector('.cell5').innerText === '') {
-    if (currentPlayer === 'X') {
-      document.querySelector('.cell5').innerText = currentPlayer
-    } else {
-      document.querySelector('.cell5').innerText = currentPlayer
-    }
-    switchPlayers()
-    gameStatus()
-  }
-}
-
-document.querySelector('.cell6').onclick = function () {
-  if (gameActive && document.querySelector('.cell6').innerText === '') {
-    if (currentPlayer === 'X') {
-      document.querySelector('.cell6').innerText = currentPlayer
-    } else {
-      document.querySelector('.cell6').innerText = currentPlayer
-    }
-    switchPlayers()
-    gameStatus()
-  }
-}
-
-document.querySelector('.cell7').onclick = function () {
-  if (gameActive && document.querySelector('.cell7').innerText === '') {
-    if (currentPlayer === 'X') {
-      document.querySelector('.cell7').innerText = currentPlayer
-    } else {
-      document.querySelector('.cell7').innerText = currentPlayer
-    }
-    switchPlayers()
-    gameStatus()
-  }
-}
-
-document.querySelector('.cell8').onclick = function () {
-  if (gameActive && document.querySelector('.cell8').innerText === '') {
-    if (currentPlayer === 'X') {
-      document.querySelector('.cell8').innerText = currentPlayer
-    } else {
-      document.querySelector('.cell8').innerText = currentPlayer
-    }
-    switchPlayers()
-    gameStatus()
-  }
-}
-
-document.querySelector('.cell9').onclick = function () {
-  if (gameActive && document.querySelector('.cell9').innerText === '') {
-    if (currentPlayer === 'X') {
-      document.querySelector('.cell9').innerText = currentPlayer
-    } else {
-      document.querySelector('.cell9').innerText = currentPlayer
-    }
     switchPlayers()
     gameStatus()
   }
